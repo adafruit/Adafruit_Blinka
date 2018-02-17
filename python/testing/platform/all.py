dd6fbb097d55861cf02cb985da72084dc2375343
Cefn Hoile <github.com@cefn.com>
2018-02-17 02:05:31 +0000

    First tests working on ESP8266

--- python/testing/platform/all.py
+++ python/testing/platform/all.py
@@ -1,11 +1,11 @@
 """Tests which should run on Micropython or CPython regardless of platform"""
-import unittest
+from testing import test_module
+import testing.mcp
+import testing.microcontroller
 
 def main():
-#    suite = unittest.TestSuite()
-#    suite.tests.append()
-    unittest.main("testing.mcp")
-    unittest.main("testing.microcontroller")
+    test_module(testing.mcp)
+    test_module(testing.microcontroller)
 
 if __name__ == "__main__":
     main()
