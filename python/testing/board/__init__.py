9c8d8fadc935d3770b603cd533bb85d56c570071
Cefn Hoile <github.com@cefn.com>
2018-02-18 20:52:14 +0000

    Moved pin_count out of board into proper place in microcontroller hierarchy

--- python/testing/board/__init__.py
+++ python/testing/board/__init__.py
@@ -4,8 +4,6 @@ import board
 if agnostic.board == "feather_m0_express":
     default_pin = board.D5
     led_pin = board.D13
-    pin_count = 38
 elif agnostic.board == "feather_huzzah":
     default_pin = board.GPIO4
     led_pin = board.GPIO2
-    pin_count = 10
