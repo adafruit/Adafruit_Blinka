e4b695e7352f118bb4b2e9c7d1e7afedcc1a1bf7
Cefn Hoile <github.com@cefn.com>
2018-02-18 01:40:46 +0000

    Now applicable to all platforms

--- python/testing/platform/all/microcontroller.py
+++ python/testing/platform/all/microcontroller.py
@@ -1,15 +1,15 @@
 import unittest
 import agnostic
 
-
 class TestMicrocontrollerModule(unittest.TestCase):
 
-
     def test_pins_exist(self):
+        """The microcontroller module should contain pin references"""
         import microcontroller
         import microcontroller.pin as pin
+        from testing.platform import pin_count
         entries = [getattr(pin, key) for key in dir(pin)]
         # is this filter line needed? any other types valid in pin module?
         entries = list(filter(lambda val: type(val) is microcontroller.Pin, entries))
-        if agnostic.platform == "esp8266":
-            self.assertTrue(len(entries) == 10)
+        self.assertTrue(len(entries) > 0)
+        self.assertTrue(len(entries) == pin_count)
