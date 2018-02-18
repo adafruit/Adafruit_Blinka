bdef7a8d9ed13a161ad5b983fe26b28c6e8b842c
Cefn Hoile <github.com@cefn.com>
2018-02-18 20:52:37 +0000

    More consistent naming of board,microcontroller,implementation

--- python/testing/implementation/all/microcontroller.py
+++ python/testing/implementation/all/microcontroller.py
@@ -6,8 +6,8 @@ class TestMicrocontrollerModule(unittest.TestCase):
     def test_pins_exist(self):
         """The microcontroller module should contain pin references"""
         import microcontroller
-        import microcontroller.pin as pin
-        from testing.platform import pin_count
+        from microcontroller import pin
+        from testing.board import pin_count
         entries = [getattr(pin, key) for key in dir(pin)]
         # is this filter line needed? any other types valid in pin module?
         entries = list(filter(lambda val: type(val) is microcontroller.Pin, entries))
