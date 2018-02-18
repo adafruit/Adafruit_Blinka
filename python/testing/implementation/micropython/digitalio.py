bd28c12d0ba940da2d93f3d13705fd806b5bfe6d
Cefn Hoile <github.com@cefn.com>
2018-02-18 20:57:28 +0000

    Updated for new testing structure

--- python/testing/implementation/micropython/digitalio.py
+++ python/testing/implementation/micropython/digitalio.py
@@ -3,20 +3,15 @@
     but which are not architecture-specific.
 """
 import unittest
-import agnostic
-import board
-
-if agnostic.board == "feather_m0_express":
-    LEDPIN = board.D13
-else:
-    raise NameError("No LED for {}".format(agnostic.platform))
+import digitalio
+from testing.board import default_pin
 
 class TestDigitalInOut(unittest.TestCase):
 
 
     def test_context_manager(self):
         """Deinitialisation is triggered by __exit__()"""
-        dio = create_pin()
+        dio = digitalio.DigitalInOut(default_pin)
         self.assertIsNotNone(dio._pin)
         with dio:
             pass
