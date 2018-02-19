1a0b0807eac28baaa1cf391adff40c6d593a34f4
Cefn Hoile <github.com@cefn.com>
2018-02-19 14:08:07 +0000

    rely on utime directly.

--- python/testing/profiling/__init__.py
+++ python/testing/profiling/__init__.py
@@ -1,11 +1,11 @@
 def time_function(f):
-    from agnostic import time
+    import utime
     """From http://docs.micropython.org/en/latest/esp8266/reference/speed_python.html"""
     myname = str(f).split(' ')[1]
     def new_func(*args, **kwargs):
-        t = time.ticks_us()
+        t = utime.ticks_us()
         result = f(*args, **kwargs)
-        delta = time.ticks_diff(time.ticks_us(), t)
+        delta = utime.ticks_diff(utime.ticks_us(), t)
         print('Function {} Time = {:6.3f}ms'.format(myname, delta/1000))
         return result
     return new_func
