bfb68396c772cb756b89d0b7db91d2faef72e8f5
Cefn Hoile <github.com@cefn.com>
2018-02-19 14:05:08 +0000

    led_hardwired flag to skip interactive questioning about wiring LEDs

--- python/testing/board/__init__.py
+++ python/testing/board/__init__.py
@@ -4,6 +4,11 @@ import board
 if agnostic.board == "feather_m0_express":
     default_pin = board.D5
     led_pin = board.D13
+    led_hardwired = True
 elif agnostic.board == "feather_huzzah":
     default_pin = board.GPIO4
     led_pin = board.GPIO2
+    led_hardwired = True
+    pull_down_support = False
+elif agnostic.board == "pyboard":
+    raise NotImplementedError
