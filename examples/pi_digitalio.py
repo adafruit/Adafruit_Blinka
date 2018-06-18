e0483776bb8e21376a300a75e5108a282df86f7f
ladyada <limor@ladyada.net>
2018-06-17 20:31:33 -0400

    pi digitalio example

--- examples/pi_digitalio.py
+++ examples/pi_digitalio.py
@@ -3,18 +3,10 @@ import time
 sys.path.append('/home/pi/Adafruit_Micropython_Blinka/src')
 sys.path.append('/home/pi/Adafruit_Python_GPIO')
 
-#from Adafruit_GPIO import Platform
-#print("Platform = ", Platform.platform_detect(), Platform.pi_version())
+import board
+import digitalio
 
 print("hello blinka!")
-
-from adafruit_blinka.agnostic import board as agnostic_board
-print("Found system type: %s (sys.plaform %s implementation %s) " % (agnostic_board, sys.platform, sys.implementation.name))
-
-import board
-print("board contents: ", dir(board))
-
-import digitalio
 
 led = digitalio.DigitalInOut(board.D4)
 led.direction = digitalio.Direction.OUTPUT
