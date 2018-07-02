2d93c728e80f530c3bca789452bf7f56d7ddaf0a
Scott Shawcroft <scott@tannewt.org>
2018-07-02 09:12:02 -0700

    Fix microcontroller imports.

--- src/microcontroller/__init__.py
+++ src/microcontroller/__init__.py
@@ -1,7 +1,7 @@
 """Microcontroller pins"""
 
-from adafruit_blinka import Enum, agnostic
-from adafruit_blinka.agnostic import board as board_id
+from adafruit_blinka import Enum
+from adafruit_blinka.agnostic import board_id, platform
 
 class Pin(Enum):
     """Reference Pin object"""
@@ -23,11 +23,11 @@ class Pin(Enum):
 # We intentionally are patching into this namespace so skip the wildcard check.
 # pylint: disable=unused-wildcard-import,wildcard-import
 
-if agnostic.microcontroller == "esp8266":
+if platform == "esp8266":
     from adafruit_blinka.microcontroller.esp8266 import *
-elif agnostic.microcontroller == "stm32":
+elif platform == "stm32":
     from adafruit_blinka.microcontroller.stm32 import *
-elif agnostic.microcontroller == "linux":
+elif platform == "linux":
     if board_id == "raspi_3" or board_id == "raspi_2":
         from adafruit_blinka.microcontroller.raspi_23 import *
     else:
--- src/microcontroller/pin.py
+++ src/microcontroller/pin.py
@@ -5,12 +5,12 @@ from adafruit_blinka import agnostic
 # We intentionally are patching into this namespace so skip the wildcard check.
 # pylint: disable=unused-wildcard-import,wildcard-import
 
-if agnostic.microcontroller == "esp8266":
+if agnostic.platform == "esp8266":
     from adafruit_blinka.microcontroller.esp8266.pin import *
-elif agnostic.microcontroller == "stm32":
+elif agnostic.platform == "stm32":
     from adafruit_blinka.microcontroller.stm32.pin import *
-elif agnostic.microcontroller == "linux":
-    if agnostic.board == "raspi_3" or agnostic.board == "raspi_2":
+elif agnostic.platform == "linux":
+    if agnostic.board_id == "raspi_3" or agnostic.board_id == "raspi_2":
         from adafruit_blinka.microcontroller.raspi_23.pin import *
     else:
         raise NotImplementedError("Board not supported: ", agnostic.board)
