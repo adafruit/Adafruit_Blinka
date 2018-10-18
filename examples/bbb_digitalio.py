25fbf2027f06ec9de82d13c205b2d766aa11dbb7
Drew Fustini <drew@pdp7.com>
2018-10-18 16:14:13 +0000

    fix style issues in BeagleBone example

--- examples/bbb_digitalio.py
+++ examples/bbb_digitalio.py
@@ -1,7 +1,7 @@
 # Example of blinking LED on BeagleBone Black
 # https://www.adafruit.com/product/1876
 #
-# Wire the circuit as follows: 
+# Wire the circuit as follows:
 # 1) connect anode (+) lead of LED to P9.12 pin
 # 2) connect cathode (-) lead to 1K Ohm resistor
 # 3) connect that 1K Ohm resistor to DGND (P9.1)
