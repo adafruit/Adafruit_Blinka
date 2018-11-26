2269a132fa710c1d1e89a3ec16df4a19bb51a72e
ladyada <limor@ladyada.net>
2018-11-26 00:14:57 -0500

    woops cleanup

--- src/pulseio.py
+++ src/pulseio.py
@@ -4,17 +4,3 @@ from adafruit_blinka.agnostic import board_id
 
 if board_id == "raspi_2" or board_id == "raspi_3":
     from adafruit_blinka.microcontroller.raspi_23.pulseio.PulseIn import PulseIn as PulseIn
-
-"""
-class PulseIn:
-    def __init__(self, pin, maxlen=2, idle_state=False):
-        self.pulsein = _PulseIn.PulseIn(pin, maxlen, idle_state=idle_state)
-
-    def deinit(self):
-        self.pulsein.deinit()
-    def pause(self):
-        self.pulsein.pause()
-    
-    def resume(self, trigger_duration=0):
-        self.pulsein(trigger_duration)
-"""
