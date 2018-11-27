b28207af9734c6c5fd9909549583ed96a69e6633
Brennen Bearnes <bbearnes@gmail.com>
2018-11-27 13:57:50 -0700

    gpiod pins: maybe handle pull up / down; tidy some comments

--- src/adafruit_blinka/microcontroller/generic_linux/pin.py
+++ src/adafruit_blinka/microcontroller/generic_linux/pin.py
@@ -1,6 +1,4 @@
 import gpiod
-# GPIO.setmode(GPIO.BCM)    # Use BCM pins D4 = GPIO #4
-# GPIO.setwarnings(False)   # shh!
 
 # Pins dont exist in CPython so...lets make our own!
 class Pin:
@@ -11,7 +9,7 @@ class Pin:
     PULL_NONE = 0
     PULL_UP = 1
     PULL_DOWN = 2
-    _CONSUMER = 'Adafruit-Blinka'
+    _CONSUMER = 'adafruit_blinka'
 
     id = None
     _value = LOW
@@ -19,7 +17,8 @@ class Pin:
 
     def __init__(self, pin_number):
         self.id = pin_number
-        self._chip = gpiod.Chip("gpiochip0") # FIXME presumably varies by system?
+        # FIXME: Presumably this might vary by system:
+        self._chip = gpiod.Chip("gpiochip0")
         self._line = self._chip.get_line(int(pin_number))
 
     def __repr__(self):
@@ -31,33 +30,37 @@ class Pin:
     def init(self, mode=IN, pull=None):
         if mode != None:
             if mode == self.IN:
+                flags = ()
+                if pull != None:
+                    if pull == self.PULL_UP:
+                        flags = (gpiod.LINE_REQ_FLAG_ACTIVE_LOW)
+                    elif pull == self.PULL_DOWN:
+                        flags = (gpiod.LINE_REQ_FLAG_ACTIVE_HIGH)
+                    else:
+                        raise RuntimeError("Invalid pull for pin: %s" % self.id)
+
                 self._mode = self.IN
-                self._line.request(consumer=self._CONSUMER, type=gpiod.LINE_REQ_DIR_IN)
-                # GPIO.setup(self.id, GPIO.IN)
+                self._line.request(
+                    consumer=self._CONSUMER,
+                    type=gpiod.LINE_REQ_DIR_IN,
+                    flags=flags
+                )
+
             elif mode == self.OUT:
+                if pull != None:
+                    raise RuntimeError("Cannot set pull resistor on output")
                 self._mode = self.OUT
                 self._line.request(consumer=self._CONSUMER, type=gpiod.LINE_REQ_DIR_OUT)
-                # GPIO.setup(self.id, GPIO.OUT)
+
             else:
                 raise RuntimeError("Invalid mode for pin: %s" % self.id)
-        if pull != None:
-            if self._mode != self.IN:
-                raise RuntimeError("Cannot set pull resistor on output")
-            if pull == self.PULL_UP:
-                GPIO.setup(self.id, GPIO.IN, pull_up_down=GPIO.PUD_UP) # XXX
-            elif pull == self.PULL_DOWN:
-                GPIO.setup(self.id, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # XXX
-            else:
-                raise RuntimeError("Invalid pull for pin: %s" % self.id)       
 
     def value(self, val=None):
         if val != None:
             if val in (self.LOW, self.HIGH):
                 self._value = val
                 self._line.set_value(val)
-                # GPIO.output(self.id, val)
             else:
                 raise RuntimeError("Invalid value for pin")
         else:
             return self._line.get_value()
-            # return GPIO.input(self.id)
