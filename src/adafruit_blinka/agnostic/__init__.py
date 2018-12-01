031c39f2511a2db0913d6f0410056e3b71c47cc5
ladyada <limor@ladyada.net>
2018-12-01 15:27:42 -0500

    orange pi GPIO led blinky

--- src/adafruit_blinka/agnostic/__init__.py
+++ src/adafruit_blinka/agnostic/__init__.py
@@ -21,16 +21,27 @@ if platform is not None:
         platform = "stm32"
         board_id = "pyboard"
     elif platform == "linux":
-        from Adafruit_GPIO import Platform
-        if Platform.platform_detect() == Platform.RASPBERRY_PI:
-            if Platform.pi_version() == 1:
-                board_id = "raspi_1"
-            elif Platform.pi_version() == 2:
-                board_id = "raspi_2"
-            elif Platform.pi_version() == 3:
-                board_id = "raspi_3"
-        elif Platform.platform_detect() == Platform.BEAGLEBONE_BLACK:
-            board_id = "beaglebone_black"
+        import re
+        # we're going to redo the Platform detection, this is a terrible hack
+        # for now.
+        try:
+            # lets see if we're an armbian board
+            for line in open("/etc/armbian-release", 'r'):
+                #print(line)
+                m = re.search('BOARD=(.*)', line)
+                if m:
+                    board_id = m.group(1)
+        except:
+            from Adafruit_GPIO import Platform
+            if Platform.platform_detect() == Platform.RASPBERRY_PI:
+                if Platform.pi_version() == 1:
+                    board_id = "raspi_1"
+                elif Platform.pi_version() == 2:
+                    board_id = "raspi_2"
+                elif Platform.pi_version() == 3:
+                    board_id = "raspi_3"
+            elif Platform.platform_detect() == Platform.BEAGLEBONE_BLACK:
+                board_id = "beaglebone_black"
 
 implementation = sys.implementation.name
 if implementation == "micropython":
---
+++ src/adafruit_blinka/microcontroller/allwinner_h3/pin.py
@@ -0,0 +1,33 @@
+from adafruit_blinka.microcontroller.generic_linux.libgpiod_pin import Pin
+
+PA0 = Pin(0)
+PA1 = Pin(1)
+PA2 = Pin(2)
+PA3 = Pin(3)
+PA6 = Pin(6)
+PA7 = Pin(7)
+PA8 = Pin(8)
+PA9 = Pin(9)
+PA10 = Pin(10)
+PA11 = Pin(11)
+PA12 = Pin(12)
+PA13 = Pin(13)
+PA14 = Pin(14)
+PA18 = Pin(18)
+PA19 = Pin(19)
+PA20 = Pin(20)
+PA21 = Pin(21)
+
+PC0 = Pin(64)
+PC1 = Pin(65)
+PC2 = Pin(66)
+PC3 = Pin(67)
+PC4 = Pin(68)
+PC7 = Pin(71)
+
+PD14 = Pin(110)
+
+PG6 = Pin(198)
+PG7 = Pin(199)
+PG8 = Pin(200)
+PG9 = Pin(201)
