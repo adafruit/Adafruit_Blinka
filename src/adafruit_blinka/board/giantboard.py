d3e1970b899f913ad65be9e0b3059e1fbacf92a9
Christopher Alessandro <calessandro0827@gmail.com>
2018-12-04 10:44:57 -0700

    Adjusted pin names to be similar to allwinner_h3 and orangepipc definitions

--- src/adafruit_blinka/board/giantboard.py
+++ src/adafruit_blinka/board/giantboard.py
@@ -7,12 +7,12 @@ PD24 = pin.PD24
 PD22 = pin.PD22
 PD19 = pin.PD19
 
-SPI0_SCLK = pin.PA14
-SPI0_MOSI = pin.PA15
-SPI0_MISO = pin.PA16
+SCK = pin.PA14
+MOSI = pin.PA15
+MISO = pin.PA16
 
-UART1_RX = pin.PD2
-UART1_TX = pin.PD3
+RX = pin.PD2
+TX = pin.PD3
 
 PD13 = pin.PD12
 PD31 = pin.PD31
@@ -21,5 +21,6 @@ PB7 = pin.PB7
 PB1 = pin.PB1
 PB5 = pin.PB5
 PB3 = pin.PB3
-TWI0_SCL = pin.PC0
-TWI0_SDA = pin.PB31
+
+SCL = pin.PC0
+SDA = pin.PB31
--- src/adafruit_blinka/microcontroller/sama5d2/pin.py
+++ src/adafruit_blinka/microcontroller/sama5d2/pin.py
@@ -7,12 +7,12 @@ PD24 = Pin(120)
 PD22 = Pin(118)
 PD19 = Pin(115)
 
-SPI0_SCLK = Pin(14)
-SPI0_MOSI = Pin(15)
-SPI0_MISO = Pin(16)
+SPI0_SCLK = PA14
+SPI0_MOSI = PA15
+SPI0_MISO = PA16
 
-UART1_RX = Pin(98)
-UART1_TX = Pin(99)
+UART1_RX = PD2
+UART1_TX = PD3
 
 PD13 = Pin(109)
 PD31 = Pin(128)
@@ -21,8 +21,8 @@ PB7 = Pin(38)
 PB1 = Pin(33)
 PB5 = Pin(37)
 PB3 = Pin(35)
-TWI0_SCL = Pin(64)
-TWI0_SDA = Pin(63)
+TWI0_SCL = PC0
+TWI0_SDA = PB31
 
 i2cPorts = ( (0, TWI0_SCL, TWI0_SDA), )
 # ordered as spiId, sckId, mosiId, misoId
