efaff70c9e806804ef3fc03ba4b746f09b86b7fd
Christopher Alessandro <calessandro0827@gmail.com>
2018-12-03 17:00:02 -0700

    Move Platform from Adafruit_GPIO and use "cat /proc/device-tree/model" to determin device.

--- src/adafruit_blinka/agnostic/__init__.py
+++ src/adafruit_blinka/agnostic/__init__.py
@@ -13,35 +13,27 @@ platform = sys.platform
 board_id = None
 
 if platform is not None:
-    if platform == "esp8266":  # TODO more conservative board-guessing
-        board_id = "feather_huzzah"
-    elif platform == "samd21":
-        board_id = "feather_m0_express"
-    elif platform == "pyboard":
-        platform = "stm32"
-        board_id = "pyboard"
-    elif platform == "linux":
-        import re
-        # we're going to redo the Platform detection, this is a terrible hack
-        # for now.
-        try:
-            # lets see if we're an armbian board
-            for line in open("/etc/armbian-release", 'r'):
-                #print(line)
-                m = re.search('BOARD=(.*)', line)
-                if m:
-                    board_id = m.group(1)
-        except:
-            from Adafruit_GPIO import Platform
-            if Platform.platform_detect() == Platform.RASPBERRY_PI:
-                if Platform.pi_version() == 1:
-                    board_id = "raspi_1"
-                elif Platform.pi_version() == 2:
-                    board_id = "raspi_2"
-                elif Platform.pi_version() == 3:
-                    board_id = "raspi_3"
-            elif Platform.platform_detect() == Platform.BEAGLEBONE_BLACK:
-                board_id = "beaglebone_black"
+	if platform == "esp8266":  # TODO more conservative board-guessing
+		board_id = "feather_huzzah"
+	elif platform == "samd21":
+		board_id = "feather_m0_express"
+	elif platform == "pyboard":
+		platform = "stm32"
+		board_id = "pyboard"
+	elif platform == "linux":
+		import Platform
+		if Platform.platform_detect() == Platform.RASPBERRY_PI:
+			if Platform.pi_version() == 1:
+				board_id = "raspi_1"
+			elif Platform.pi_version() == 2:
+				board_id = "raspi_2"
+			elif Platform.pi_version() == 3:
+				board_id = "raspi_3"
+		elif Platform.platform_detect() == Platform.BEAGLEBONE_BLACK:
+			board_id = "beaglebone_black"
+		elif Platform.platform_detect() == Platform.ORANGEPIPC:
+			board_id = "orangepipc"
+			
 
 implementation = sys.implementation.name
 if implementation == "micropython":
