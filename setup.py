2e93a5e1c767f5686102db79cc5632d0041cdb75
Brennen Bearnes <brennen@adafruit.com>
2018-12-17 22:41:33 -0700

    remove Adafruit-GPIO requirement

--- setup.py
+++ setup.py
@@ -33,7 +33,6 @@ setup(
     # If your package is a single module, use this instead of 'packages':
     py_modules=['bitbangio', 'board', 'busio', 'digitalio', 'micropython', 'neopixel_write'],
     install_requires=[
-        'Adafruit-GPIO',
         'Adafruit-PlatformDetect',
         "RPi.GPIO; platform_machine=='armv7l' or platform_machine=='armv6l'",
         "rpi_ws281x>=4.0.0; platform_machine=='armv7l' or platform_machine=='armv6l'"
