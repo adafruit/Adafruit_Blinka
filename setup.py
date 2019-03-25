81790ff48cfc2a68f198997d71313384c661bb3d
Matthew Wilkes <git@matthewwilkes.name>
2019-03-25 19:53:20 +0000

    Limit sysv_ipc to non-Windows installs
    This dependency is compatible with "most (all?) Unixes", Windows support
    is unofficial and only under Cygwin. Cygwin and WSL Python installs have
    values of `platform.system()` other than Windows, therefore this prevents
    installlation of `sysv_ipc` under standard Windows. This will cause `PulseIn`
    to generate import errors on Windows, but as there is no Windows driver this
    is likely not a problem. It will, however, allow for dependency resolution
    and packaging to work on Windows.

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
         "RPi.GPIO; platform_machine=='armv7l' or platform_machine=='armv6l'",
         "rpi_ws281x>=4.0.0; platform_machine=='armv7l' or platform_machine=='armv6l'",
         "spidev; sys_platform=='linux'",
-        "sysv_ipc"
+        "sysv_ipc; platform_system != 'Windows'"
     ],
     license='MIT',
     classifiers=[
