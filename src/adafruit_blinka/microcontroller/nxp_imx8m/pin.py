0b9e819a6206fe0ea87673d27fc6e3293f3e1809
ladyada <limor@ladyada.net>
2019-05-12 16:28:25 -0400

    add a periphery-based sysfs pwmout object - tested with LED and servo

--- src/adafruit_blinka/microcontroller/nxp_imx8m/pin.py
+++ src/adafruit_blinka/microcontroller/nxp_imx8m/pin.py
@@ -7,6 +7,10 @@ I2C3_SCL = Pin(146) # GPIO5_IO18
 I2C3_SDA = Pin(147) # GPIO5_IO19
 
 
+PWM1 = Pin((0, 1))      # GPIO1_IO01
+PWM2 = Pin((0, 13))     # GPIO1_IO13
+PWM3 = Pin((0, 14))    # GPIO1_IO14
+
 GPIO6 = Pin((0, 6))     # GPIO1_IO6
 GPIO7 = Pin((0, 7))     # GPIO1_IO7
 GPIO8 = Pin((0, 8))     # GPIO1_IO8
@@ -24,6 +28,8 @@ ECSPI1_SS0 = Pin(133)  # GPIO5_IO9
 i2cPorts = ( (1, I2C2_SCL, I2C2_SDA), (2, I2C3_SCL, I2C3_SDA),)
 # ordered as spiId, sckId, mosiId, misoId
 spiPorts = ( (32766, ECSPI1_SCLK, ECSPI1_MOSI, ECSPI1_MISO), )
+# SysFS pwm outputs, pwm channel and pin in first tuple
+pwmOuts = ( ((0, 0), PWM1), ((1, 0), PWM2), ((2, 0), PWM3), )
 
 # UART1_TXD/RXD on /dev/ttymxc0
 # UART3_TXD/RXD not available (?)
--- src/pulseio.py
+++ src/pulseio.py
@@ -2,3 +2,5 @@ from adafruit_blinka.agnostic import detector
 
 if detector.board.any_raspberry_pi:
     from adafruit_blinka.microcontroller.bcm283x.pulseio.PulseIn import PulseIn
+if detector.board.any_coral_board:
+    from adafruit_blinka.microcontroller.generic_linux.sysfs_pwmout import PWMOut
