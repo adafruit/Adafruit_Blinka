34520b447774e7d448f9440d1be962eaab3682f3
Christopher Alessandro <calessandro0827@gmail.com>
2019-05-18 19:00:20 -0700

    Revert "Commenting these out gets rid of errors and allows the pwm to export."
    This reverts commit f599d177e9baa31e507c6633681dcfc65b73c759.

--- src/adafruit_blinka/microcontroller/generic_linux/sysfs_pwmout.py
+++ src/adafruit_blinka/microcontroller/generic_linux/sysfs_pwmout.py
@@ -90,12 +90,12 @@ class PWMOut(object):
         except IOError as e:
             raise PWMError(e.errno, "Exporting PWM pin: " + e.strerror)
 
-        #self._set_enabled(False)
+        self._set_enabled(False)
         
         # Look up the period, for fast duty cycle updates
         self._period = self._get_period()
 
-        #self.duty_cycle = 0
+        self.duty_cycle = 0
 
         # set frequency
         self.frequency = freq
