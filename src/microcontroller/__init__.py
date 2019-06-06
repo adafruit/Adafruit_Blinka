3e3bb1147102d5c669de9d2062fb292589ec2849
Melissa LeBlanc-Williams <melissa@Melissas-MacBook-Pro.local>
2019-06-06 14:09:09 -0700

    linting

--- src/microcontroller/__init__.py
+++ src/microcontroller/__init__.py
@@ -43,7 +43,7 @@ elif chip_id == ap_chip.T186:
 elif chip_id == ap_chip.T194:
     from adafruit_blinka.microcontroller.tegra.t194 import *
 elif chip_id == ap_chip.S905:
-    from adafruit_blinka.microcontroller.amlogic.s905.pin import *    
+    from adafruit_blinka.microcontroller.amlogic.s905.pin import *
 elif chip_id == ap_chip.IMX8MX:
     from adafruit_blinka.microcontroller.nxp_imx8m import *
 else:
