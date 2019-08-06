5189e2d75d524d4eb6b44ac3465aec3917353fde
Melissa LeBlanc-Williams <melissa@adafruit.com>
2019-08-06 15:47:48 -0700

    PR Changes, organized mc, removed duplicates

--- src/board.py
+++ src/board.py
@@ -85,12 +85,12 @@ elif board_id == ap_board.JETSON_NANO:
 elif board_id == ap_board.CORAL_EDGE_TPU_DEV:
     from adafruit_blinka.board.coral_edge_tpu import *
 
-#elif board_id == ap_board.ODROID_C2:
-    #from adafruit_blinka.board.odroidc2 import *
+elif board_id == ap_board.ODROID_C2:
+    from adafruit_blinka.board.odroidc2 import *
 
 elif board_id == ap_board.ODROID_N2:
    from adafruit_blinka.board.odroidn2 import *
-# error above
+
 elif board_id == ap_board.DRAGONBOARD_410C:
     from adafruit_blinka.board.dragonboard_410c import *
 
