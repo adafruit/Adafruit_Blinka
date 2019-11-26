e89ea27c8b1db795949b3538dc10bec6117399ad
Thea Flowers <me@thea.codes>
2019-11-25 18:07:14 -0800

    Add stubs for micropython.natve, viper, and asm_thumb

--- src/micropython.py
+++ src/micropython.py
@@ -1,2 +1,14 @@
 def const(x):
     return x
+
+
+def native(f):
+    return f
+
+
+def viper(f):
+    raise SyntaxError("invalid micropython decorator")
+
+
+def asm_thumb(f):
+    raise SyntaxError("invalid micropython decorator")
