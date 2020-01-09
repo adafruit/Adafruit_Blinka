3339c0ad512a3a2dd77b0b92a232bd018f59844f
yeyeto2788 <juanernestobiondi@gmail.com>
2020-01-09 20:24:11 +0100

    Fix error on microcontroller.pin when importing `pin`

--- src/microcontroller/pin.py
+++ src/microcontroller/pin.py
@@ -39,6 +39,6 @@ elif chip_id == ap_chip.BINHO:
 elif chip_id == ap_chip.MCP2221:
     from adafruit_blinka.microcontroller.mcp2221.pin import *
 elif chip_id == ap_chip.A64:
-    from adafruit_blinka.microcontroller.allwinner.pin import *
+    from adafruit_blinka.microcontroller.allwinner.a64.pin import *
 else:
     raise NotImplementedError("Microcontroller not supported: ", chip_id)
