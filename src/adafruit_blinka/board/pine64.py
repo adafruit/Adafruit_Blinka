c2997be92c305b0899a1c34ef66f563aa5278e5f
Melissa LeBlanc-Williams <melissa@adafruit.com>
2020-01-10 17:56:57 -0800

    Changed pin names from GPIO to D because it feel better

--- src/adafruit_blinka/board/pine64.py
+++ src/adafruit_blinka/board/pine64.py
@@ -2,43 +2,50 @@
 
 from adafruit_blinka.microcontroller.allwinner.a64 import pin
 
-GPIO_2 = pin.PH3
-GPIO_3 = pin.PH2
-GPIO_4 = pin.PL10
-GPIO_5 = pin.PH5
-GPIO_6 = pin.PH6
-GPIO_7 = pin.PH7
-GPIO_8 = pin.PC3
-GPIO_9 = pin.PC1
-GPIO_10 = pin.PC0
-GPIO_11 = pin.PC2
-GPIO_12 = pin.PC4
-GPIO_13 = pin.PC5
-GPIO_14 = pin.PB0
-GPIO_15 = pin.PB1
-GPIO_16 = pin.PC6
-GPIO_17 = pin.PC7
-GPIO_18 = pin.PC8
-GPIO_19 = pin.PC9
-GPIO_20 = pin.PC10
-GPIO_21 = pin.PC11
-GPIO_22 = pin.PC12
-GPIO_23 = pin.PC13
-GPIO_24 = pin.PC14
-GPIO_25 = pin.PC15
-GPIO_26 = pin.PC16
-GPIO_27 = pin.PH9
+D2 = pin.PH3
+D3 = pin.PH2
+D4 = pin.PL10
+D5 = pin.PH5
+D6 = pin.PH6
+D7 = pin.PH7
+D8 = pin.PC3
+D9 = pin.PC1
+D10 = pin.PC0
+D11 = pin.PC2
+D12 = pin.PC4
+D13 = pin.PC5
+D14 = pin.PB0
+D15 = pin.PB1
+D16 = pin.PC6
+D17 = pin.PC7
+D18 = pin.PC8
+D19 = pin.PC9
+D20 = pin.PC10
+D21 = pin.PC11
+D22 = pin.PC12
+D23 = pin.PC13
+D24 = pin.PC14
+D25 = pin.PC15
+D26 = pin.PC16
+D27 = pin.PH9
 
-SDA = GPIO_2
-SCL = GPIO_3
+SDA = D2
+SCL = D3
 
 SCL2 = pin.PL8
 SDA2 = pin.PL9
 
-SCLK = GPIO_11
-MOSI = GPIO_10
-MISO = GPIO_9
-CS = GPIO_8
+SCLK = D11
+MOSI = D10
+MISO = D9
+CS = D8
+SCK = SCLK
 
-UART_TX = GPIO_14
-UART_RX = GPIO_15
+UART_TX = D14
+UART_RX = D15
+
+UART3_TX = pin.PD0
+UART3_RX = pin.PD1
+
+UART4_TX = pin.PD2
+UART4_RX = pin.PD3
