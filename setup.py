4777de83304539da5c0e52a16dc4ba58f1d7f0f8
Melissa LeBlanc-Williams <melissa@adafruit.com>
2020-02-14 09:56:36 -0800

    Update Python required version to 3.5

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: Implementation :: MicroPython',
     ],
 )
