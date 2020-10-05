bd66d6f572f6b6451d62cc6b1d80502b095933ef
Simon-Pierre Allaire <spa05@acuitysso.com>
2020-10-05 12:14:32 -0400

    Add redefined classes

--- examples/mcps_busio_i2c.py
+++ examples/mcps_busio_i2c.py
@@ -2,16 +2,33 @@ import time
 import sys
 import board
 import busio
+import hid
 
-print("hello blinka!")
+from adafruit_blinka.microcontroller.mcp2221.mcp2221 import mcp2221 as _mcp2221
+from adafruit_blinka.microcontroller.mcp2221.mcp2221 import MCP2221 as _MCP2221
+from adafruit_blinka.microcontroller.mcp2221.i2c import I2C as _MCP2221I2C
 
-i2c = busio.I2C(board.SCL, board.SDA)
 
-print("I2C devices found: ", [hex(i) for i in i2c.scan()])
+class MCP2221(_MCP2221): 
+    def __init__(self, address):
+        self._hid = hid.device()
+        self._hid.open_path(address)
+        print("Connected to "+str(address))
+        self._gp_config = [0x07] * 4  # "don't care" initial value
+        for pin in range(4):
+            self.gp_set_mode(pin, self.GP_GPIO)  # set to GPIO mode
+            self.gpio_set_direction(pin, 1)  # set to INPUT
 
-if not 0x18 in i2c.scan():
-    print("Didn't find MCP9808")
-    sys.exit()
+
+class MCP2221I2C(_MCP2221I2C):
+    def __init__(self, mcp2221, *, frequency=1000000):
+        self._mcp2221 = mcp2221
+        self._mcp2221.i2c_configure(frequency)
+
+
+class I2C(busio.I2C):
+    def __init__(self, mcp2221_i2c, *, frequency=1000000):
+        self._i2c = mcp2221_i2c
 
 
 def temp_c(data):
