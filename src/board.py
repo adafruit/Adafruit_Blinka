43c1b03bba1553399352d5b9324813a63cefdd03
twa127 <46624596+twa127@users.noreply.github.com>
2020-10-30 16:09:27 +0000

    Update board.py

--- src/board.py
+++ src/board.py
@@ -96,7 +96,10 @@ elif board_id == ap_board.ORANGE_PI_PLUS_2E:
 elif board_id == ap_board.ORANGE_PI_2:
     from adafruit_blinka.board.orangepi.orangepipc import *
 
-elif board_id == ap_board.GIANT_BOARD:
+elif board_id == ap_board.BANANA_PI_M2_ZERO:
+    from adafruit_blinka.board.bananapi.bpim2zero import *
+
+    elif board_id == ap_board.GIANT_BOARD:
     from adafruit_blinka.board.giantboard import *
 
 elif board_id == ap_board.JETSON_TX1:
