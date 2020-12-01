84dd0709887d2a452693e3c3552a7ee97684aa6e
yeyeto2788 <juanernestobiondi@gmail.com>
2020-12-01 14:43:36 +0100

    Define all valid values for env variable

--- src/adafruit_blinka/constants.py
+++ src/adafruit_blinka/constants.py
@@ -1,3 +1,4 @@
 import os
 
-SUPPRESS_WARNINGS = os.getenv("SUPPRESS_WARNINGS", False)
+TRUE_VALUES = [True, "True", "true", "1", 1]
+SUPPRESS_WARNINGS = os.getenv("SUPPRESS_WARNINGS", False) in TRUE_VALUES
