3b57f987031e811e8d3cf1c257fb5eafd528e6f0
twa127 <46624596+twa127@users.noreply.github.com>
2021-01-14 08:33:40 +0000

    Create orangepizeroplus.py

---
+++ src/adafruit_blinka/board/orangepi/orangepizeroplus.py
@@ -0,0 +1,38 @@
+"""Pin definitions for the Orange Pi Zero Plus."""
+
+from adafruit_blinka.microcontroller.allwinner.h5 import pin
+
+PA12 = pin.PA12
+SDA = pin.PA12
+PA11 = pin.PA11
+SCL = pin.PA11
+PA6 = pin.PA6
+PWM1 = pin.PA6
+PA1 = pin.PA1
+UART2_RX = pin.PA1
+PA0 = pin.PA0
+UART2_TX = pin.PA0
+PA3 = pin.PA3
+UART2_CTS = pin.PA3
+PA15 = pin.PA15
+PA16 = pin.PA16
+PA14 = pin.PA14
+SPI1_CLK = pin.PA14
+PG6 = pin.PG6
+UART1_TX = pin.PG6
+PG7 = pin.PG7
+UART1_RX = pin.PG7
+PA7 = pin.PA7
+PA19 = pin.PA19
+TWI1_SDA = pin.PA19
+PA18 = pin.PA18
+TWI1_SCK = pin.PA18
+PA2 = pin.PA2
+UART2_RTS = pin.PA2
+PA13 = pin.PA13
+SPI1_CS = pin.PA13
+PA10 = pin.PA10
+
+SCLK = pin.PA14
+MOSI = pin.PA15
+MISO = pin.PA16
