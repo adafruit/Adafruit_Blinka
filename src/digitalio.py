96453d4ab42d7ccc21be051d69f89da82d695591
twa127 <46624596+twa127@users.noreply.github.com>
2021-02-06 23:09:47 +0000

    Update digitalio.py

--- src/digitalio.py
+++ src/digitalio.py
@@ -69,6 +69,8 @@ elif detector.chip.MT8167:
     from adafruit_blinka.microcontroller.mt8167.pin import Pin
 elif detector.chip.H5:
     from adafruit_blinka.microcontroller.allwinner.h5.pin import Pin
+elif detector.chip.H616:
+    from adafruit_blinka.microcontroller.allwinner.h616.pin import Pin
 
 from adafruit_blinka import Enum, ContextManaged
 
