24d6724573147a401c894e1ebfa3b968791ece36
twa127 <46624596+twa127@users.noreply.github.com>
2021-02-07 17:50:02 +0000

    Update pin.py

--- src/adafruit_blinka/microcontroller/allwinner/h616/pin.py
+++ src/adafruit_blinka/microcontroller/allwinner/h616/pin.py
@@ -1,2 +1,21 @@
 """Allwinner H616 Pin Names"""
 from adafruit_blinka.microcontroller.generic_linux.libgpiod_pin import Pin
+
+PC5  = Pin((1, 69))
+PC6  = Pin((1, 70))
+PC7 = Pin((1, 71))
+PC8 = Pin((1, 72))
+PC9 = Pin((1, 73))
+PC10 = Pin((1, 74))
+PC11 = Pin((1, 75))
+PC14 = Pin((1, 78))
+PC15 = Pin((1, 79))
+
+PH2 = Pin((1, 226))
+PH3 = Pin((1, 227))
+PH4 = Pin((1, 228))
+PH5 = Pin((1, 229))
+PH6 = Pin((1, 230))
+PH7 = Pin((1, 231))
+PH8 = Pin((1, 232))
+PH9 = Pin((1, 233))
