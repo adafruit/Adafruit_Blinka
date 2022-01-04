2f03480aec2585ef5f412925f06c0746267cb753
twa127 <46624596+twa127@users.noreply.github.com>
2022-01-04 13:19:11 +0000

    Add files via upload

---
+++ src/adafruit_blinka/board/orangepi/orangepi3.py
@@ -0,0 +1,31 @@
+"""Pin definitions for the Orange Pi 3."""
+
+from adafruit_blinka.microcontroller.allwinner.h6 import pin
+
+PD26 = pin.PD26
+SDA = pin.PD26
+PD25 = pin.PD25
+SCL = pin.PD25
+PD22 = pin.PD22
+PL02 = pin.PL02
+PL03 = pin.PL03
+PD24 = pin.PD24
+UART3_RX = pin.PD24
+PD18 = pin.PD18
+PD23 = pin.PD23
+UART3_TX = pin.PD23
+PL10 = pin.PL10
+PD15 = pin.PD15
+PD16 = pin.PD16
+PH05 = pin.PH05
+PH06 = pin.PH06
+PD21 = pin.PD21
+PH04 = pin.PH04
+SPI1_CLK = pin.PH04
+PH03 = pin.PH03
+SPI1_CS = pin.PH03
+PL08 = pin.PL08
+
+SCLK = pin.PH04
+MOSI = pin.PH05
+MISO = pin.PH06
