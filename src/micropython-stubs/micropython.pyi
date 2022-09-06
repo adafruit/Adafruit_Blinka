1ed771f1cca5f23273e93a509ef003e363810985
Jeff Epler <jepler@gmail.com>
2022-09-06 11:57:30 -0500

    Provide stubs for micropython module
    This allows use of micropython.const to typecheck without
    making any changes to the .py files themselves.

---
+++ src/micropython-stubs/micropython.pyi
@@ -0,0 +1,29 @@
+# SPDX-FileCopyrightText: 2021 Melissa LeBlanc-Williams for Adafruit Industries
+#
+# SPDX-License-Identifier: MIT
+"""
+`micropython` - MicroPython Specific Decorator Functions
+========================================================
+
+* Author(s): cefn
+"""
+
+from typing import Callable, TypeVar, Any, NoReturn
+
+Fun = TypeVar("Fun", bound=Callable[..., Any])
+
+def const(x: int) -> int:
+    "Emulate making a constant"
+    return x
+
+def native(f: Fun) -> Fun:
+    "Emulate making a native"
+    return f
+
+def viper(f: Fun) -> NoReturn:
+    "User is attempting to use a viper code emitter"
+    raise SyntaxError("invalid micropython decorator")
+
+def asm_thumb(f: Fun) -> NoReturn:
+    "User is attempting to use an inline assembler"
+    raise SyntaxError("invalid micropython decorator")
