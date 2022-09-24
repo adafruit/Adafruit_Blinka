67a34259146b18204a8770b253e20a1eaa690e86
Ryan L <ryestar101@gmail.com>
2022-09-24 21:33:24 +0100

    Add pin control support for the A20. Given that it is pin compatible with the A10, it keeps things simple to use the already created pin map file for the A10.

--- src/microcontroller/pin.py
+++ src/microcontroller/pin.py
@@ -33,6 +33,8 @@ elif chip_id == ap_chip.JH71x0:
     from adafruit_blinka.microcontroller.starfive.JH71x0.pin import *
 elif chip_id == ap_chip.SUN4I:
     from adafruit_blinka.microcontroller.allwinner.a10.pin import *
+elif chip_id == ap_chip.SUN7I:
+    from adafruit_blinka.microcontroller.allwinner.a10.pin import *
 elif chip_id == ap_chip.SUN8I:
     from adafruit_blinka.microcontroller.allwinner.h3.pin import *
 elif chip_id == ap_chip.H3:
@@ -84,6 +86,8 @@ elif chip_id == ap_chip.LPC4330:
 elif chip_id == ap_chip.MCP2221:
     from adafruit_blinka.microcontroller.mcp2221.pin import *
 elif chip_id == ap_chip.A10:
+    from adafruit_blinka.microcontroller.allwinner.a10.pin import *
+elif chip_id == ap_chip.A20:
     from adafruit_blinka.microcontroller.allwinner.a10.pin import *
 elif chip_id == ap_chip.A64:
     from adafruit_blinka.microcontroller.allwinner.a64.pin import *
