e54e0c606364fe22445b7d8efd6cbd18951f90db
Ryan L <ryestar101@gmail.com>
2022-11-03 16:11:47 +0000

    carry out some formatting checks with pylint

--- src/adafruit_blinka/board/linksprite/pcduino3.py
+++ src/adafruit_blinka/board/linksprite/pcduino3.py
@@ -1,9 +1,10 @@
 """Pin definitions for the Pcduino3."""
 from adafruit_blinka.microcontroller.allwinner.a10 import pin
 
-#Pin wise this is the same as the Pcduino2 but features a daul core A20 rather than a single core A10
+# Pin wise this is the same as the Pcduino2 but features a daul core A20
+# # rather than a single core A10
 
-#J11 Header
+# J11 Header
 D0 = pin.PI19
 D1 = pin.PI18
 D2 = pin.PH7
@@ -13,7 +14,7 @@ D5 = pin.PB2
 D6 = pin.PI3
 D7 = pin.PH9
 
-#J8 Header
+# J8 Header
 D8 = pin.PH10
 D9 = pin.PH5
 D10 = pin.PI10
@@ -23,14 +24,14 @@ D13 = pin.PI11
 SDA = pin.PB21
 SCL = pin.PB20
 
-#J9 Header
-#A0 & A1 are LRADC pins with only 6-bit resolution and unlike other pins are only limited to 2v
+# J9 Header
+# A0 & A1 are LRADC pins with only 6-bit resolution and unlike other pins are only limited to 2v
 A2 = pin.XP_TP
 A3 = pin.XN_TP
 A4 = pin.YP_TP
 A5 = pin.YN_TP
 
-#P7 Header
+# P7 Header
 SCLK = D13
 SCK = SCLK
 MOSI = D11
@@ -38,7 +39,7 @@ MISO = D12
 CS = D10
 
 
-#P6 Header
+# P6 Header
 D22 = pin.PC20
 SCLK2 = D22
 SCK2 = SCLK2
@@ -49,7 +50,7 @@ MISO2 = D20
 D21 = pin.PC19
 CS2 = D21
 
-#P10 Header
+# P10 Header
 D14 = pin.PH11
 D15 = pin.PH12
 D16 = pin.PH13
@@ -63,7 +64,7 @@ UART5_RX = D2
 PWM0 = D5
 PWM1 = D6
 
-#Misc
+# Misc
 BACK_SW = pin.PH17 #Three buttons featured on the board
 HOME_SW = pin.PH18
 MENU_SW = pin.PH19
--- src/adafruit_blinka/microcontroller/allwinner/a10/pin.py
+++ src/adafruit_blinka/microcontroller/allwinner/a10/pin.py
@@ -1,26 +1,26 @@
 """Allwinner A10 pin names"""
 from adafruit_blinka.microcontroller.generic_linux.libgpiod_pin import Pin
 
-PB2 = Pin(34) #PB2/PWM0 
+PB2 = Pin(34) # PB2/PWM0
 PWM0 = PB2
-PB20 = Pin(52) #PB20/TWI2_SDL
+PB20 = Pin(52) # PB20/TWI2_SDL
 TWI2_SCL = PB20
-PB21 = Pin(53) #PB20/TWI2_SDA
+PB21 = Pin(53) # PB20/TWI2_SDA
 TWI2_SDA = PB21
 
-PC19 = Pin(83) #PC19/SPI2_CS
+PC19 = Pin(83) # PC19/SPI2_CS
 SPI2_CS = PC19
-PC20 = Pin(84) #PC20/SPI2_SCLK
+PC20 = Pin(84) # PC20/SPI2_SCLK
 SPI2_SCLK = PC20
-PC21 = Pin(85) #PC21/SPI2_MOSI
+PC21 = Pin(85) # PC21/SPI2_MOSI
 SPI2_MOSI = PC21
-PC22 = Pin(86) #PC22/SPI2_MISO
+PC22 = Pin(86) # PC22/SPI2_MISO
 SPI2_MISO = PC22
 
 PH5 = Pin(229)
-PH6 = Pin(230) #PH6/UART5_TX
+PH6 = Pin(230) # PH6/UART5_TX
 UART5_TX = PH6
-PH7 = Pin(231) #PH7/UART5_RX
+PH7 = Pin(231) # PH7/UART5_RX
 UART5_RX = PH7
 PH8 = Pin(232)
 PH9 = Pin(233)
@@ -35,11 +35,11 @@ PH17 = Pin(241)
 PH18 = Pin(242)
 PH19 = Pin(243)
 
-PI3 = Pin(259) #PI3/PWM1
+PI3 = Pin(259) # PI3/PWM1
 PWM1 = PI3
-PI10 = Pin(266) #PI10/SPI0_CS/UART5_TX
-SPI0_CS = PI10 
-PI11 = Pin(267) #PI11/SPI0_SCLK/UART5_RX
+PI10 = Pin(266) # PI10/SPI0_CS/UART5_TX
+SPI0_CS = PI10
+PI11 = Pin(267) # PI11/SPI0_SCLK/UART5_RX
 SPI0_SCLK = PI11
 PI12 = Pin(268)
 SPI0_MOSI = PI12
@@ -47,15 +47,15 @@ UART6_TX = PI12
 PI13 = Pin(269)
 SPI0_MISO = PI13
 UART6_RX = PI13
-PI18 = Pin(274) #PI18/UART2_TX
+PI18 = Pin(274) # PI18/UART2_TX
 UART2_TX = PI18
-PI19 = Pin(275) #PI19/UART2_RX
+PI19 = Pin(275) # PI19/UART2_RX
 UART2_RX = PI19
 
-XP_TP = 1 #Allwinner touch panel controller as able to operate as four seperate adc chanels
-XN_TP = 2 #offering 12-bit resolution.
+XP_TP = 1 # Allwinner touch panel controller which can be configured to operate
+XN_TP = 2 # as four seperate adc chanels, providing 12-bit resolution.
 YP_TP = 3
-YN_TP = 4 
+YN_TP = 4
 
 # Ordered as i2cId, SCL, SDA
 i2cPorts =((2, TWI2_SCL, TWI2_SDA),)
