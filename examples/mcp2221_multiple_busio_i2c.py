bb0ea63fb6c4f24ac388bc521ad2f2b55cca37c1
Ezio Melotti <ezio.melotti@gmail.com>
2022-12-04 21:42:08 -0700

    Improve examples/mcp2221_multiple_busio_i2c.py.

--- examples/mcp2221_multiple_busio_i2c.py
+++ examples/mcp2221_multiple_busio_i2c.py
@@ -1,24 +1,30 @@
 # SPDX-FileCopyrightText: 2022, The Blinka Authors.
 #
 # SPDX-License-Identifier: MIT
-import hid
+
 import busio
 
+from adafruit_blinka.microcontroller.mcp2221.mcp2221 import MCP2221
 
 # Usage:
 # $ python mcp2221_multiple_busio_i2c.py
-# I2C devices found:  []
-# I2C devices found:  ['0x70']
+# 2 MCP2221 found: [b'2-3.3:1.2', b'2-3.2:1.2']
+# I2C devices found on 2-3.3:1.2: ['0x77']
+# I2C devices found on 2-3.2:1.2: ['0x58', '0x61']
 
+# # MCP2221.available_paths() is roughly equivalent to
+# import hid
+# MCP2221_VID = 0x04D8
+# MCP2221_PID = 0x00DD
+# addresses = [mcp["path"] for mcp in hid.enumerate(MCP2221_VID, MCP2221_PID)]
 
-MCP2221_VID = 0x04D8
-MCP2221_PID = 0x00DD
+addresses = MCP2221.available_paths()
+print(f'{len(addresses)} MCP2221(s) found: {addresses}')
 
 i2c_busses = []
-addresses = [mcp["path"] for mcp in hid.enumerate(MCP2221_VID, MCP2221_PID)]
-
 for address in addresses:
     i2c_busses.append(busio.I2C(bus_id=address))
 
-for i2c in i2c_busses:
-    print("I2C devices found: ", [hex(i) for i in i2c.scan()])
+for address, i2c in zip(addresses, i2c_busses):
+    print(f'I2C devices found on {address.decode()}:',
+          [hex(i) for i in i2c.scan()])
