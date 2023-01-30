a0f7ff1eb324df0e1e4b255943af95da72cdb09a
Enrico <etrevismail@gmail.com>
2023-01-30 20:24:02 +0100

    change name to rock pi 3a

--- src/board.py
+++ src/board.py
@@ -229,8 +229,8 @@ elif board_id == ap_board.ONION_OMEGA2:
 elif board_id == ap_board.RADXA_CM3:
     from adafruit_blinka.board.radxa.radxacm3 import *
 
-elif board_id == ap_board.RADXA_3A:
-    from adafruit_blinka.board.radxa.radxa3a import *
+elif board_id == ap_board.ROCK_PI_3A:
+    from adafruit_blinka.board.radxa.rockpi3a import *
 
 elif board_id == ap_board.RADXA_ZERO:
     from adafruit_blinka.board.radxa.radxazero import *
