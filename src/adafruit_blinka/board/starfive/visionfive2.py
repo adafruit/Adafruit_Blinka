afcaf41e064bacb2416d2c6cd2fc05c36529789b
DarkMechanikum <v.shtarev08@gmail.com>
2024-07-04 23:31:03 +0300

    fixes

--- src/adafruit_blinka/board/starfive/visionfive2.py
+++ src/adafruit_blinka/board/starfive/visionfive2.py
@@ -5,29 +5,20 @@
 from adafruit_blinka.microcontroller.starfive.JH7110 import pin
 
 
-D3 = pin.D3
-D5 = pin.D5
 D7 = pin.D7
-D8 = pin.D8
-D10 = pin.D10
 D11 = pin.D11
 D12 = pin.D12
 D13 = pin.D13
 D15 = pin.D15
 D16 = pin.D16
 D18 = pin.D18
-D19 = pin.D19
-D21 = pin.D21
 D22 = pin.D22
-D23 = pin.D23
 D24 = pin.D24
 D26 = pin.D26
 D27 = pin.D27
 D28 = pin.D28
 D29 = pin.D29
 D31 = pin.D31
-D32 = pin.D32
-D33 = pin.D33
 D35 = pin.D35
 D36 = pin.D36
 D37 = pin.D37
@@ -45,3 +36,7 @@ SPI_SCLK = pin.SPI_SCLK
 # UART
 UART_TX = pin.UART_TX
 UART_RX = pin.UART_RX
+
+# PWM
+PWM1 = pin.PWM1
+PWM2 = pin.PWM2
