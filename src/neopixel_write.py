527c4543196b4d1ad600a7caf7058251bb09e3ee
Melissa LeBlanc-Williams <melissa@adafruit.com>
2025-01-15 13:19:14 -0800

    Add Pi500 and CM5 Support

--- src/neopixel_write.py
+++ src/neopixel_write.py
@@ -16,7 +16,7 @@ import sys
 from adafruit_blinka.agnostic import detector, board_id
 
 if detector.board.any_raspberry_pi:
-    if board_id == "RASPBERRY_PI_5":
+    if detector.board.any_raspberry_pi_5_board:
         import adafruit_raspberry_pi5_neopixel_write as _neopixel
     else:
         from adafruit_blinka.microcontroller.bcm283x import neopixel as _neopixel
